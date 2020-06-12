5d2cfc2a6ce2773724fefca1ea8d234601b04db4
Aman Srivastava <amansrivastava905@gmail.com>
2020-06-12 22:16:24 +0530

    refined ui
    added some pages and features.

--- script/index.js
+++ script/index.js
@@ -33,3 +33,22 @@ document.querySelector(".burger").addEventListener("click",function(){
 	
 });
 
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    var panel = this.nextElementSibling;
+    
+
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+  });
+}
+
